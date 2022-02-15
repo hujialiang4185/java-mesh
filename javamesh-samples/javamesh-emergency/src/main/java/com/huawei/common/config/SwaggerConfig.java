225b814dbb1ac504e70879381b743a9496733704
ssyyzs <ssyyzs1314@163.com>
2022-02-15 11:37:07 +0800

    swagger依赖整理

--- javamesh-samples/javamesh-emergency/src/main/java/com/huawei/common/config/SwaggerConfig.java
+++ javamesh-samples/javamesh-emergency/src/main/java/com/huawei/common/config/SwaggerConfig.java
@@ -36,12 +36,11 @@ public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)  // DocumentationType.SWAGGER_2 固定的，代表swagger2
-//                .groupName("分布式任务系统") // 如果配置多个文档的时候，那么需要配置groupName来分组标识
-            .apiInfo(apiInfo()) // 用于生成API信息
-            .select() // select()函数返回一个ApiSelectorBuilder实例,用来控制接口被swagger做成文档
-            .apis(RequestHandlerSelectors.basePackage("com.huawei.emergency.controller")) // 用于指定扫描哪个包下的接口
-            .paths(PathSelectors.any())// 选择所有的API,如果你想只为部分API生成文档，可以配置这里
+        return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.huawei.emergency.controller"))
+            .paths(PathSelectors.any())
             .build();
     }
 
