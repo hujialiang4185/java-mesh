d5f09adcfaeb80ae21dfcb38f12ff7ad8d9ca3f5
h30009881 <hujialiang4@huawei.com>
2022-03-03 11:20:57 +0800

    修复修改脚本报错问题

--- javamesh-samples/javamesh-user/src/main/resources/create-table.sql
+++ javamesh-samples/javamesh-user/src/main/resources/create-table.sql
@@ -16,27 +16,30 @@ CREATE TABLE IF NOT EXISTS `emergency_group`
     INDEX         `group_name`(`group_name`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
-CREATE TABLE IF NOT EXISTS `emergency_user_role`
-(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    `role_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    `group_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-    PRIMARY KEY(`id`) USING BTREE,
-    INDEX `fk_group_user`(`group_name`) USING BTREE,
-    CONSTRAINT `fk_group_user` FOREIGN KEY(`group_name`) REFERENCES `emergency_group` (`group_name`) ON DELETE RESTRICT ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS `emergency_user`  (
+                                   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                                   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+                                   `last_modified_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '最后修改时间',
+                                   `enabled` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '是否启用',
+                                   `role_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名',
+                                   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+                                   `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+                                   `nick_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '昵称',
+                                   `group_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                   PRIMARY KEY (`id`) USING BTREE,
+                                   INDEX `fk_user_group`(`group_name`) USING BTREE,
+                                   CONSTRAINT `fk_user_group` FOREIGN KEY (`group_name`) REFERENCES `emergency_group` (`group_name`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of emergency_auth
 -- ----------------------------
-insert into `emergency_user_role` (user_name, role_name)
-select 'admin', 'ADMIN'
+insert into `emergency_user` (created_date,last_modified_date,enabled,role_name,password,user_name,nick_name)
+select CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,'T','ADMIN','$2a$10$KE0VERWzRwYJIQn17XoePe0/iokyCqKlnX0t.C90Okg7eDsy0Fi/a','admin','admin'
 FROM DUAL
 WHERE NOT EXISTS(SELECT user_name, role_name
-                 FROM `emergency_user_role`
-                 WHERE user_name = 'admin'
-                   AND role_name = 'ADMIN');
+                 FROM `emergency_user`
+                 WHERE user_name = 'admin');
 
 insert into `emergency_auth` (role_name, auth_name)
 select 'ADMIN', 'admin'
