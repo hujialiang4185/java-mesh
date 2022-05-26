2a476a866efeebe376edf335885dd356e5643250
y30010171 <yangdeshan@huawei.com>
2022-05-26 17:16:34 +0800

    增加emergency_agent dto类

---
+++ javamesh-samples/javamesh-emergency/src/main/java/com/huawei/emergency/dto/ServerAgentInfoDTO.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huawei.emergency.dto;
+
+import com.huawei.emergency.entity.EmergencyServer;
+
+import lombok.Data;
+
+/**
+ * agent in emergency server
+ *
+ * @author y30010171
+ * @since 2022-05-24
+ **/
+@Data
+public class ServerAgentInfoDTO extends EmergencyServer {
+    private Integer agentId;
+    private String agentName;
+    private String agentIp;
+    private Integer agentPort;
+    private String agentStatus;
+}
